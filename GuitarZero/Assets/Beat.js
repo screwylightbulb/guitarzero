e9326d397b67607f5f475ad3e9295fa678153146
screwylighbulb <jaco@screwylightbulb.com>
2015-04-19 11:22:36 +0000

    Beats

--- GuitarZero/Assets/Beat.js
+++ GuitarZero/Assets/Beat.js
@@ -1,9 +1,89 @@
 ﻿#pragma strict
 
-function Start () {
+private var nextBeat : float  = 0;
+private var beatRate : float =  0.12;
 
+var counter : int = 0;
+
+var adrum : AudioSource; 
+var asnare : AudioSource; 
+var abass : AudioSource; 
+var aguit : AudioSource; 
+
+var sequence : String = "";
+
+var countObj : UnityEngine.UI.Text;
+
+function Start () {
+    countObj = GameObject.Find("Count").GetComponent(UI.Text);
 }
 
 function Update () {
-
+    
+    if (Time.time > nextBeat) {
+        nextBeat = Time.time + beatRate;
+        Beat();
+    }
+    
+    if (Input.GetKeyDown("h")) {
+        aguit.pitch =  1.1;
+        aguit.Play();
+        sequence = sequence + counter.ToString();
+        CheckSeq();
+    }
+    if (Input.GetKeyDown("j")) {
+        aguit.pitch =  1.2;
+        aguit.Play();
+        sequence = sequence + counter.ToString();
+        CheckSeq();
+    }
+    if (Input.GetKeyDown("k")) {
+        aguit.pitch =  1.3;
+        aguit.Play();
+        sequence = sequence + counter.ToString();
+        CheckSeq();
+    }
+    if (Input.GetKeyDown("l")) {
+        aguit.pitch =  1.4;
+        aguit.Play();
+        sequence = sequence + counter.ToString();
+        CheckSeq();
+    }
+    
 }
+
+function CheckSeq() {
+    Debug.Log(sequence);
+}
+
+function Beat(){ 
+    
+    if (counter == 0) {
+        Debug.Log("start");
+    }
+
+    if (counter % 2 == 0) {
+        abass.Play();
+    }
+
+    if (counter % 4 == 0) {
+        adrum.Play();
+    }
+    
+    if (counter % 8 == 0) {
+        asnare.Play();
+    }
+    
+    counter++;
+    
+    if (counter == 15) {
+        sequence = "";
+    }
+    if (counter == 16) {
+        counter = 0;
+    }
+    
+    countObj.text = counter.ToString();
+}
+
+
